1c4df82c7a8c12c53fa3976d86185e264c6cc408
김승찬 <s24041@gsm.hs.kr>
2024-10-02 23:29:27 +0900

    forEach 대신 stream, map 사용

--- skills/src/main/java/hellobackend/skills/config/auth/PrincipalDetails.java
+++ skills/src/main/java/hellobackend/skills/config/auth/PrincipalDetails.java
@@ -2,23 +2,22 @@ package hellobackend.skills.config.auth;
 
 import hellobackend.skills.model.User;
 import lombok.Getter;
-import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import java.util.ArrayList;
 import java.util.Collection;
+import java.util.stream.Collectors;
 
 @Getter
-@Setter
 public class PrincipalDetails implements UserDetails {
 
     private final User user;
 
-    public PrincipalDetails(User user){
+    public PrincipalDetails(User user) {
         this.user = user;
     }
+
     @Override
     public String getPassword() {
         return user.getPassword();
@@ -51,11 +50,8 @@ public class PrincipalDetails implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        Collection<GrantedAuthority> authorities = new ArrayList<>();
-        user.getRoleList().forEach(role -> {
-            authorities.add(new SimpleGrantedAuthority(role));
-        });
-        return authorities;// UserDetails가 이미 GrantedAuthority 목록을 반환
+        return user.getRoleList().stream()
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
     }
 }
-
