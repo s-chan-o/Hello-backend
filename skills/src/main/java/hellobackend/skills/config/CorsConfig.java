e0b46b314f5adeac548fd19864c276ed01662841
김승찬 <s24041@gsm.hs.kr>
2024-10-02 23:47:51 +0900

    CorsConfiguration 객체 먼저 생성

--- skills/src/main/java/hellobackend/skills/config/CorsConfig.java
+++ skills/src/main/java/hellobackend/skills/config/CorsConfig.java
@@ -11,7 +11,6 @@ public class CorsConfig {
 
     @Bean
     public CorsFilter corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowCredentials(true);
@@ -19,6 +18,7 @@ public class CorsConfig {
         config.addAllowedHeader("*");//모든 header 허용
         config.addAllowedMethod("*");//모든 http 메서드 허용
 
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/api/**", config);
 
         return new CorsFilter(source);
