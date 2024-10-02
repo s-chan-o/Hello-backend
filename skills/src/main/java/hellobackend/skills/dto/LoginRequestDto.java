3c03c9d1c084c9b0ae50e897d2fcb42809cba135
김승찬 <s24041@gsm.hs.kr>
2024-10-02 23:59:16 +0900

    @NotEmpty 어노테이션 사용
    사용자 이름과 비밀번호가 필수임을 명시했습니다.

--- skills/src/main/java/hellobackend/skills/dto/LoginRequestDto.java
+++ skills/src/main/java/hellobackend/skills/dto/LoginRequestDto.java
@@ -1,5 +1,6 @@
 package hellobackend.skills.dto;
 
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,6 +8,9 @@ import lombok.Setter;
 @Setter
 public class LoginRequestDto {
 
+    @NotEmpty(message = "사용자 이름은 필수입니다.")
     private String username;
+
+    @NotEmpty(message = "비밀번호는 필수입니다.")
     private String password;
 }
