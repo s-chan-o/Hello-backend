2159443f3b8535301a98475b82fbdf07b98a5834
김승찬 <s24041@gsm.hs.kr>
2024-10-02 23:39:09 +0900

    표준 방식 설정

--- skills/src/main/java/hellobackend/skills/config/jwt/JwtProperties.java
+++ skills/src/main/java/hellobackend/skills/config/jwt/JwtProperties.java
@@ -5,5 +5,5 @@ public interface JwtProperties {
     String SECRET = "SECRET"; // 비밀 키
     long EXPIRATION_TIME = 864_000_000; // 10일
     String HEADER_STRING = "Authorization";
-    String TOKEN_PREFIX = "Bearer "; //
+    String TOKEN_PREFIX = "Bearer ";
 }
