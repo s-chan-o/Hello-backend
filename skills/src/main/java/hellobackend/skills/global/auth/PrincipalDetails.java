cac6dca68e7d7ea34caddabfae5f1cb2b40e0f86
김승찬 <s24041@gsm.hs.kr>
2024-10-05 01:11:21 +0900

    All 어노테이션 삭제

--- skills/src/main/java/hellobackend/skills/global/auth/PrincipalDetails.java
+++ skills/src/main/java/hellobackend/skills/global/auth/PrincipalDetails.java
@@ -11,7 +11,6 @@ import java.util.Collection;
 import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
-@AllArgsConstructor
 @Getter
 public class PrincipalDetails implements UserDetails {
 
