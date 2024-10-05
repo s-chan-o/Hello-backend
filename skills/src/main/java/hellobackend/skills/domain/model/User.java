87c0034c3070557a30425725774cb82d891684ab
김승찬 <s24041@gsm.hs.kr>
2024-10-05 16:05:07 +0900

    죽은 import 삭제

--- skills/src/main/java/hellobackend/skills/domain/model/User.java
+++ skills/src/main/java/hellobackend/skills/domain/model/User.java
@@ -6,8 +6,6 @@ import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
