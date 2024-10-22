6b5b25530b96107b01b6f95b616035472995a65b
김승찬 <s24041@gsm.hs.kr>
2024-10-22 20:13:12 +0900

    api를 service에서 호출하여 처리하도록 수정

--- skills/src/main/java/hellobackend/skills/domain/controller/RestApiController.java
+++ skills/src/main/java/hellobackend/skills/domain/controller/RestApiController.java
@@ -1,11 +1,11 @@
 package hellobackend.skills.domain.controller;
 
-import hellobackend.skills.domain.repository.UserRepository;
-import hellobackend.skills.global.auth.PrincipalDetails;
+import hellobackend.skills.domain.dto.UserDto;
 import hellobackend.skills.domain.model.User;
+import hellobackend.skills.domain.service.UserService;
+import hellobackend.skills.global.auth.PrincipalDetails;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,8 +15,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class RestApiController {
 
-    private final UserRepository userRepository;
-    private final BCryptPasswordEncoder passwordEncoder;
+    private final UserService userService;
 
     @GetMapping("home")
     public String home() {
@@ -28,28 +27,40 @@ public class RestApiController {
         PrincipalDetails principal = (PrincipalDetails) authentication.getPrincipal();
         System.out.println("UserID: " + principal.getUser().getId());
         System.out.println("Username: " + principal.getUser().getUsername());
-
         return "user";
     }
 
-    // 매니저 or 어드민 접근 가능
     @GetMapping("manager/reports")
     public String reports() {
         return "reports";
     }
 
-    // 어드민 접근 가능
     @GetMapping("admin/users")
     public List<User> users() {
-        return userRepository.findAll();
+        return userService.getAllUsers();
     }
 
-
+    // 회원가입
     @PostMapping("join")
-    public String join(@RequestBody User user) {
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
-        user.setRoles(List.of(User.Role.USER));
-        userRepository.save(user);
-        return "회원가입 완료";
+    public String join(@RequestBody UserDto userDto) {
+        return userService.signUp(userDto);
+    }
+
+    // 로그인
+    @PostMapping("login")
+    public String login(@RequestBody UserDto userDto) {
+        return userService.login(userDto);
+    }
+
+    // 로그아웃
+    @PostMapping("logout")
+    public String logout() {
+        return userService.logout();
+    }
+
+    // 토큰 재발급
+    @PostMapping("token/reissue")
+    public String reissueToken(@RequestParam String oldToken) {
+        return userService.reissueToken(oldToken);
     }
 }
